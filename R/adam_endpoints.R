bd46478568a157a55c6e42ff0d11f03c4d9047e1
jagadishkatam <jagadish.katam@gmail.com>
2025-02-20 16:29:59 -0500

    updated  files

---
+++ R/adam_endpoints.R
@@ -0,0 +1,47 @@
+
+library(tidyverse)
+library(DT)
+library(httr2)
+library(jsonlite)
+
+adam <- readRDS("./data/endpoint_df_links.rds") |> filter(str_detect(endpoint,'adam') & str_detect(type,'Guide'))
+
+
+link <- map_dfr(adam$endpoint, \(x){
+# API URL
+url <- paste0("https://api.library.cdisc.org/api",x,"/datastructures")
+print(url)
+# Construct the request
+req <- request(url) %>%
+  req_headers(
+    "Cache-Control" = "no-cache",
+    "api-key" = "ba3d68879a224d8090406948f8155bae",
+    "content-type" = "application/json"
+  )
+
+# Send the request and fetch response
+resp <- req %>% req_perform()
+
+# Parse JSON response
+endpoint <- resp %>% resp_body_json()
+
+return(
+  map_dfr(seq_along(endpoint$`_links`$dataStructures), \(j){
+    text <- endpoint$`_links`$dataStructures[[j]]$href
+  tibble(name=endpoint$`_links`$dataStructures[[j]]$href,
+         endpoint=str_extract(endpoint$`_links`$dataStructures[[j]]$href,'.*(?=/datastructures)'),
+         dataset = str_extract(text,'(?<=/datastructures/).*')
+         )
+})
+  
+)
+
+})
+
+
+adam_links <- adam |> left_join(link, join_by(endpoint)) |> mutate(
+  endpoint=sapply(strsplit(endpoint,'/'),\(x) x[4])
+)
+
+
+saveRDS(adam_links,"./data/adam_endpoint_df_links.rds")
